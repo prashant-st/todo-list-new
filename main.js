083ab1468b4b1bd218e0f003d30bc21d759e43ec
Sargam-Agarwal <sargam123india@gmail.com>
2022-08-05 10:26:47 +0530

    added the keyboard shortcut to select/unselect the first task

--- main.js
+++ main.js
@@ -3,34 +3,47 @@ var allTasks = document.getElementsByClassName("close");
 function addNewElement() {
     var input = document.getElementById("input field").value;
 
-    var li = document.createElement("li");
-    var txt = document.createTextNode(input);
-    li.appendChild(txt);
+    if (input.length > 0) {
 
-    var i = document.createElement("i");
-    var icon = document.createTextNode("\u2715");
+        var li = document.createElement("li");
+        var txt = document.createTextNode(input);
+        li.appendChild(txt);
 
-    i.className = "close";
-    i.appendChild(icon);
+        var i = document.createElement("i");
+        var icon = document.createTextNode("\u2715");
 
-    li.appendChild(i);
-    li.className = "tasks-list-li";
-    li.accessKey = 'a';
+        i.className = "close";
+        i.appendChild(icon);
 
-    var ul = document.getElementById("tasks-list-ul");
-    ul.appendChild(li);
+        li.appendChild(i);
+        li.className = "tasks-list-li";
 
-    document.getElementById("input field").value = "";
+        var ul = document.getElementById("tasks-list-ul");
+        ul.appendChild(li);
 
-    for (var i = 0; i < allTasks.length; i++) {
-        allTasks[i].onclick = function () {
-            var div = this.parentElement;
-            div.style.display = "none";
+        document.getElementById("input field").value = "";
+
+        for (var i = 0; i < allTasks.length; i++) {
+            allTasks[i].onclick = function () {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
         }
-    }
-    var list = document.querySelector("#tasks-list-ul");
-    list.addEventListener("click", function (ev) {
-        ev.target.classList.toggle("checked");
 
-    });
+        var list = document.querySelector("#tasks-list-ul");
+        list.addEventListener("click", function (ev) {
+            ev.target.classList.toggle("checked");
+        });
+
+        window.addEventListener('keydown', function (event) {
+            var li = this.document.querySelector(".tasks-list-li");
+            li.classList.toggle("checked");
+            console.log(event);
+        });
+
+    }
+    else {
+        alert("Enter some task");
+    }
+
 }
