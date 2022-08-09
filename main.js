1318c9764b1f06d0f652f4bb690f03b8a749c594
Sargam-Agarwal <sargam123india@gmail.com>
2022-08-09 11:52:14 +0530

    loading and rendering the list from web local storage

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// localStorage.setItem("tasks", JSON.stringify([["key1", "value1"], ["key2", "value2"]]));
+
 var count = 0;
 if (localStorage.getItem("count") != null) {
     count = localStorage.getItem("count");
@@ -15,8 +17,10 @@ function addNewElement() {
 
     if (input.length > 0) {
         var li = document.createElement("li");
+        li.className = "tasks-list-li";
         var txt = document.createTextNode(input);
         li.appendChild(txt);
+        console.log(li);
 
         var i = document.createElement("i");
         var icon = document.createTextNode("\u2715");
@@ -25,7 +29,6 @@ function addNewElement() {
         i.appendChild(icon);
 
         li.appendChild(i);
-        li.className = "tasks-list-li";
 
         var ul = document.getElementById("tasks-list-ul");
         ul.appendChild(li);
@@ -43,7 +46,6 @@ function addNewElement() {
         var list = document.querySelector("#tasks-list-ul");
         list.addEventListener("click", function (ev) {
             ev.target.classList.toggle("checked");
-
         });
 
         window.addEventListener('ArrowDown', function (event) {
@@ -63,15 +65,55 @@ function addNewElement() {
 function onSave() {
     var allTasks = [...document.querySelectorAll(".tasks-list-li")];
     console.log(allTasks);
-    allTasks.forEach((task) => {
+    allTasks.forEach((task, index) => {
         var task = {
             "text": task.innerText,
             "isDone": (task.classList.contains("checked")) ? true : false,
         }
-        localStorage.setItem("task" + count, JSON.stringify(task));
+        localStorage.setItem("task" + (index + 1), JSON.stringify(task));
     });
 }
 
 function onLoad() {
+    var ul = document.getElementById("tasks-list-ul");
+    console.log(count);
+    for (var j = 1; j <= count; j++) {
+        console.log("i:" + count);
+        if (localStorage.getItem("task" + j) != null) {
+            var task = JSON.parse(localStorage.getItem("task" + j));
+            var li = document.createElement("li");
+            li.className = "tasks-list-li";
+            if (task['isDone']) {
+                li.className = 'checked';
+            }
 
+            var textNode = document.createTextNode(task['text'].slice(0, task['text'].length - 1));
+
+            li.appendChild(textNode);
+
+            var i = document.createElement("i");
+            var icon = document.createTextNode("\u2715");
+
+            i.className = "close";
+            i.appendChild(icon);
+
+            li.appendChild(i);
+
+            ul.appendChild(li);
+
+            var allTasks = document.getElementsByClassName("close");
+            for (var i = 0; i < allTasks.length; i++) {
+                allTasks[i].onclick = function () {
+                    var div = this.parentElement;
+                    div.style.display = "none";
+                }
+            }
+
+            var list = document.querySelector("#tasks-list-ul");
+            list.addEventListener("click", function (ev) {
+                console.log("clicked");
+                ev.target.classList.toggle("checked");
+            });
+        }
+    }
 }
