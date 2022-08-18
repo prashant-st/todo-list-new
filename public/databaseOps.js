dc35b9fa2584f76eb5cccc7d620b08afc547b958
Sargam-Agarwal <sargam123india@gmail.com>
2022-08-18 15:27:33 +0530

    set up firebase realtime database for the project

---
+++ public/databaseOps.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDGPmki-WjYPZxq6sppxT_f22hPMf3Izj8",
+    authDomain: "to-do-list-5f3c8.firebaseapp.com",
+    databaseURL: "https://to-do-list-5f3c8-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "to-do-list-5f3c8",
+    storageBucket: "to-do-list-5f3c8.appspot.com",
+    messagingSenderId: "837634878572",
+    appId: "1:837634878572:web:f84e0e274534e93945bc49",
+    measurementId: "G-J9T179LP6Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
